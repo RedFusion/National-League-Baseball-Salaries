a4a8f2372277a75261736e69b6f1fe35db9911c6
RedFusion <mentozei@yandex.ru>
2015-11-15 16:42:03 +0500

    Spring
    Spring

--- src/main/java/ru/menkin/store/ISpringStorage.java
+++ src/main/java/ru/menkin/store/ISpringStorage.java
@@ -1,7 +1,9 @@
 package ru.menkin.store;
 
+import org.springframework.transaction.annotation.*;
 import ru.menkin.models.*;
 
+@Transactional(readOnly = false)
 public interface ISpringStorage extends Storage<Player>{
 
 }
--- src/main/java/ru/menkin/store/Storage.java
+++ src/main/java/ru/menkin/store/Storage.java
@@ -1,12 +1,11 @@
 package ru.menkin.store;
 
 import java.util.*;
-
+import org.springframework.transaction.annotation.Transactional;
 /**
  * @author Menkin
  * @since 12.10.2015
  */
-
 public interface Storage <Player> {
     public Collection<Player> values();
 
