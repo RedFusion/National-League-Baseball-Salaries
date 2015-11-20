2f051074a074f9028daa42e16d616260411ec7ac
RedFusion <mentozei@yandex.ru>
2015-11-20 20:54:45 +0500

    #6 Angular
    lessons

--- src/main/webapp/Angular_Lesson5/controller.js
+++ src/main/webapp/Angular_Lesson5/controller.js
@@ -1,16 +1,15 @@
 /**
  * Created by Menkin on 18.11.2015.
  */
-'use strict';
-
 /* Controllers */
 
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
-    $http.get('phones/phones.json').success(function(data) {
-        $scope.phones = data;
-    });
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        $http.get('phones/phones.json').success(function(data) {
+            $scope.phones = data;
+        });
 
-    $scope.orderProp = 'age';
-}]);
+        $scope.orderProp = 'age';
+    }]);
